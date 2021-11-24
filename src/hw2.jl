7d3ecd339dda956e3886997f7328b4ef3421c13e
lxjlu <55116031+lxjlu@users.noreply.github.com>
2021-11-24 09:55:21 +0800

    Initial commit

---
+++ src/hw2.jl
@@ -0,0 +1,14 @@
+module hw2
+
+greet() = print("Hello World!")
+
+
+function studentinfo()
+    info = Dict(
+        "name" => "Brian Jackson",
+        "Andrew ID" => "bjackso2"
+    )
+    return info
+end
+
+end # module
